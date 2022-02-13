5519dc818294e368f231bd2991dff1f4ae88238e
Jeff Workman <jworkhollr@aol.com>
2022-02-13 13:46:14 -0500

    Add project files.

---
+++ Account1/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account1 {
+
+    class Account {
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+
+        public bool Deposit(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be positive.");                                  
+            }
+            Balance = Balance + Amount;
+            return true;
+        }
+        public bool Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            if (Amount > Balance) {
+                throw new Exception("Insuffient Funds!");
+            }
+
+            Balance = Balance - Amount;
+            return true;
+            
+        }
+        public void Debug() {
+            Console.WriteLine($"{Id} | {Description} | has a Balance of {Balance:c}");
+
+        }
+    }
+}
---
+++ Account1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Account1 {
+
+    public class Program {
+
+        static void Main(string[] args) {
+
+            Account acct1 = new Account();
+            acct1.Deposit (1000);
+            acct1.Withdraw(350);
+            acct1.Debug();
+            acct1.Deposit(200);
+            acct1.Debug();
+                
+        }
+    }
+}
